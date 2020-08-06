226131e0bddcce95ecf9bd4a51c662deacfbf059
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-08-06 12:22:42 -0700

    chess board cell color

---
+++ intro/chessBoardCellColor.py
@@ -0,0 +1,25 @@
+'''
+Given two cells on the standard chess board, determine whether they have the same color or not.
+'''
+
+
+def chessBoardCellColor(cell1, cell2):
+    board = {
+        'A': 1,
+        'B': 2,
+        'C': 3,
+        'D': 4,
+        'E': 5,
+        'F': 6,
+        'G': 7,
+        'H': 8
+    }
+    total1 = board[cell1[0]] + int(cell1[1])
+    total2 = board[cell2[0]] + int(cell2[1])
+    return total1 % 2 == 0 and total2 % 2 == 0
+
+
+cell1 = "A1"
+cell2 = "C3"
+
+print(chessBoardCellColor(cell1, cell2))
