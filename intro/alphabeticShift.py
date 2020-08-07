9f382246abdb6dc9203b8189d4896b8dbefb3a39
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-08-07 15:11:18 -0700

    deposit profit

--- intro/alphabeticShift.py
+++ intro/alphabeticShift.py
@@ -2,11 +2,9 @@
 Given a string, your task is to replace each of its characters by the next one in the English alphabet; i.e. replace a with b, replace b with c, etc (z would be replaced by a).
 '''
 
-import string
-
 
 def alphabeticShift(inputString):
-    letters = string.ascii_lowercase
+    letters = 'abcdefghijklmnopqrstuvwxyz'
     inputString = list(inputString)
     for i in range(len(inputString)):
         if inputString[i] == 'z':
