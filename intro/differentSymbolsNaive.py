713343923fdce32324c3e25e2b560a78fa9a8605
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-08-10 16:11:20 -0700

    different symbol naive

---
+++ intro/differentSymbolsNaive.py
@@ -0,0 +1,14 @@
+'''
+Given a string, find the number of different characters in it.
+'''
+
+from collections import Counter
+
+
+def differentSymbolsNaive(s):
+    s_counter = Counter(s)
+    return len(s_counter)
+
+
+s: "cabca"
+print(differentSymbolsNaive(s))
