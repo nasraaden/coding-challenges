0c36aa717e522cecbad294347c1c00a9dc87f5c5
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-08-10 17:26:34 -0700

    array max consecutive sum

---
+++ intro/arrayMaxConsecutiveSum.py
@@ -0,0 +1,21 @@
+'''
+Given array of integers, find the maximal possible sum of some of its k consecutive elements.
+'''
+
+
+def arrayMaxConsecutiveSum(inputArray, k):
+    k_sum = sum(inputArray[0:k])
+    max_sum = k_sum
+
+    for i in range(k, len(inputArray)):
+        k_sum = k_sum - inputArray[i-k] + inputArray[i]
+        if k_sum > max_sum:
+            max_sum = k_sum
+
+    return max_sum
+
+
+inputArray = [2, 3, 5, 1, 6]
+k = 2
+
+print(arrayMaxConsecutiveSum(inputArray, k))
