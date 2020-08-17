5a9d9741294da1bde981636be5de60b3a7c232c2
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-08-17 13:08:39 -0700

    bishop and pawn

---
+++ intro/isBeautifulString.py
@@ -0,0 +1,5 @@
+'''
+A string is said to be beautiful if each letter in the string appears at most as many times as the previous letter in the alphabet within the string; ie: b occurs no more times than a; c occurs no more times than b; etc.
+
+Given a string, check whether it is beautiful.
+'''
